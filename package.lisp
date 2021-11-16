98b4efa8bdc19f7d45d94e1d057bd9983966e217
other-otter <93465141+other-otter@users.noreply.github.com>
2021-11-16 15:55:03 +0800

    Update package.lisp

--- package.lisp
+++ package.lisp
@@ -2,5 +2,15 @@
 
 (defpackage #:thread-party
     (:use :cl :cl-user)
-    (:export nil
+    (:export *party-queue*
+             *party-list*
+             *party-plan*
+             *party-theme*
+             make-plan
+             default-theme
+             set-theme
+             send-message
+             start-party
+             close-party
+             list-thread
     ))
