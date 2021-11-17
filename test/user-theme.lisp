04d7b8f5c338a60dcf845143dd185262752be5fa
other-otter <93465141+other-otter@users.noreply.github.com>
2021-11-17 20:00:12 +0800

    Update user-theme.lisp

--- test/user-theme.lisp
+++ test/user-theme.lisp
@@ -4,5 +4,29 @@
 
 (thread-party::start-party)
 
-(loop for i from 1 to 100 do 
-    (lparallel.queue:push-queue i thread-party::*party-queue*))
+(defun a (b)
+  (if (<= b 1)
+      nil
+      (c b (1- b))))
+
+(defun c (d e)
+  (if (= e 1)
+      (progn (push d f)
+             (values t d))
+      (if (= 0 (mod d e))
+          nil
+          (c d (1- e)))))
+
+(defun a (b thread-number)
+  (if (<= b 1)
+      nil
+      (c b (1- b))))
+
+(thread-party:set-theme #'a)
+
+(setf f nil)
+
+(loop for i from 1 to 100 do
+    (thread-party:send-message i))
+
+(symbol-value 'f)
