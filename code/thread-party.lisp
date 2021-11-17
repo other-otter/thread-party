e3233978c35621a250ccca3b7cb846f5bc026361
other-otter <93465141+other-otter@users.noreply.github.com>
2021-11-18 03:00:01 +0800

    Update thread-party.lisp

--- code/thread-party.lisp
+++ code/thread-party.lisp
@@ -40,3 +40,32 @@
 
 (defun main ()
     (start-party))
+
+(defun plan-party (party-symbol-quote &key  (party-plan (cpus:get-number-of-processors))
+                                            (party-list nil)
+                                            (party-theme #'thread-party:default-theme) 
+                                            (party-queue (lparallel.queue:make-queue)))
+    (set party-symbol-quote (list   :party-plan     party-plan
+                                    :party-list     party-list
+                                    :party-theme    party-theme
+                                    :party-queue    party-queue)))
+
+(defun hold-party (party-symbol)
+    (loop for i from 1 to (getf party-symbol :party-plan) do
+        (push 
+            (bordeaux-threads:make-thread 
+                (lambda () 
+                    (loop 
+                        (let ((the-message (lparallel.queue:pop-queue (getf party-symbol :party-queue))))
+                            (funcall (getf party-symbol :party-theme) the-message i)))))
+            (getf party-symbol :party-list))))
+
+(defun sure-party (party-symbol party-theme)
+    (setf (getf party-symbol :party-theme) party-theme))
+
+(defun take-party (party-symbol the-message)
+    (lparallel.queue:push-queue the-message (getf party-symbol :party-queue)))
+
+(defun shut-party (party-symbol)
+    (mapcar #'bordeaux-threads:destroy-thread (getf party-symbol :party-list))
+    (setf (getf party-symbol :party-list) nil))
